574a2a19050323ec1bc2792e75e4e1cf14c2d298
Ji Xiang <jx4554@gmail.com>
2017-07-29 15:03:38 +0800

    finish user signup/login/logout api

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,14 @@ Rails.application.routes.draw do
 	resources :meetups
 
 	resources :comments, only: [:create]
+
+	namespace :api, :defaults => { :format => :json } do
+		namespace :v1 do
+			
+			post "/signup" => "auth#signup"
+			post "/login" => "auth#login"
+			post "/logout" => "auth#logout"
+
+		end
+	end
 end
